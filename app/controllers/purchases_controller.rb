b55b9225cd26cf9a22d0898aa13f07ea1983020d
Jeffe <Jeffe.bernal@gmail.com>
2016-11-17 19:06:07 -0500

    in controller include de object notifybalancemailer when balance >10000

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -10,6 +10,8 @@ before_action :its_admin?, only: [:update, :dashboard, :destroy]
     purchase.status = :pending
     purchase.price = product.precio
     if product.available? && purchase.save
+      balance = Purchase.balance(current_user)
+      PurchaseNotifierMailer.notify_account_balance(current_user,balance) if balance >  10_000
       product.update_stock
       flash[:notice] = "Tu compra ha sido realizada con éxito"
     else
